1a8280c7d483d38fc024c2d8ab1b8b5f2aaf04fc
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-09-03 10:03:09 +0200

    template html preview, mechanism of finding elements of mrrt standard, mrrt section implementation

--- src/Editor.js
+++ src/Editor.js
@@ -2,28 +2,56 @@ import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import PropTypes from 'prop-types';
 import {MRRTNodeComponent} from './MRRTNodeComponent';
+import ReactDOMServer from 'react-dom/server';
 
-class MRRTOption extends MRRTNodeComponent {
-
+class MRRTSection extends MRRTNodeComponent {
+    renderInTemplateMode() {
+        let node = this.props.node;
+        return <section data-section-name={node.attribs['data-section-name']}>
+            <header className="level1">
+                {node.attribs['data-section-name']}
+            </header>
+            <p>
+                {node.children.map((n, i) => this.props.transformFunc(n, i))}
+            </p>
+        </section>;
+    }
 }
 
+MRRTSection.propTypes = {
+    transformFunc: PropTypes.func.isRequired,
+    node: PropTypes.object.isRequired,
+};
+
+
 export class Editor extends React.Component {
-
+    factoryFunctionsLib = [
+        {
+            conditionFunc: (node) => (node.type === 'tag' && node.name === 'section'),
+            makeObj: (node) => <MRRTSection mode={this.state.mode} transformFunc={this.transform}
+                                            node={node}/>,
+        },
+    ];
     transform = (node, index) => {
-        if (node.type === 'tag' && node.name === 'section') {
-            return <section>{node.attribs['data-section-name']}
-                {node.children.map((n, i) => this.transform(n, i))}
-            </section>;
+        let factObj = this.factoryFunctionsLib.find((factFunc) => factFunc.conditionFunc(node));
+        if (typeof factObj !== 'undefined') {
+            return factObj.makeObj(node, this.transform);
         }
-
     };
+
+    state = { mode: 'template' };
 
     render() {
         let templateHTMl = this.props.templateHTMl;
+        let out = ReactHtmlParser(templateHTMl, { transform: this.transform });
+        let htmlStr = ReactDOMServer.renderToStaticMarkup(out);
+
         return <div>
-            {
-                ReactHtmlParser(templateHTMl, { transform: this.transform })
-            }
+            {out}
+
+            <p>
+                {htmlStr}
+            </p>
         </div>;
     }
 }
