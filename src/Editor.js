da40fe4594a326da7ab4a1beb651cda79fbc48a6
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-09-03 11:20:26 +0200

    add mrrt text

--- src/Editor.js
+++ src/Editor.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser, {convertNodeToElement} from 'react-html-parser';
 import PropTypes from 'prop-types';
 import {MRRTNodeComponent} from './MRRTNodeComponent';
 import ReactDOMServer from 'react-dom/server';
 
 class MRRTSection extends MRRTNodeComponent {
+    constructor(props) {
+        super(props);
+        let node = props.node;
+        this.state = {
+            sectionName: node.attribs['data-section-name'],
+        };
+    }
+
     renderInTemplateMode() {
+        let state = this.state;
         let node = this.props.node;
-        return <section data-section-name={node.attribs['data-section-name']}>
-            <header className="level1">
-                {node.attribs['data-section-name']}
-            </header>
-            <p>
-                {node.children.map((n, i) => this.props.transformFunc(n, i))}
-            </p>
+        return <section data-section-name={state.sectionName}>
+            {node.children.map((n, i) => this.props.transformFunc(n, i))}
         </section>;
     }
 }
@@ -23,6 +27,27 @@ MRRTSection.propTypes = {
     node: PropTypes.object.isRequired,
 };
 
+class MRRTText extends MRRTNodeComponent {
+    constructor(props) {
+        super(props);
+        let node = props.node;
+        this.state = {
+            text: node.attribs['value'],
+        };
+    }
+
+    handleTextChange = (e) => {
+        this.setState({ text: e.target.value });
+    };
+
+    renderInTemplateMode() {
+        let state = this.state;
+        let node = this.props.node;
+        return <input value={state.text} onChange={this.handleTextChange}>
+        </input>;
+    }
+}
+
 
 export class Editor extends React.Component {
     factoryFunctionsLib = [
@@ -31,12 +56,18 @@ export class Editor extends React.Component {
             makeObj: (node) => <MRRTSection mode={this.state.mode} transformFunc={this.transform}
                                             node={node}/>,
         },
+        {
+            conditionFunc: (node) => (node.type === 'tag' && node.attribs['data-field-type'] === 'TEXT'),
+            makeObj: (node) => <MRRTText mode={this.state.mode} transformFunc={this.transform}
+                                         node={node}/>,
+        },
     ];
     transform = (node, index) => {
         let factObj = this.factoryFunctionsLib.find((factFunc) => factFunc.conditionFunc(node));
         if (typeof factObj !== 'undefined') {
             return factObj.makeObj(node, this.transform);
         }
+        return convertNodeToElement(node, index, this.transform);
     };
 
     state = { mode: 'template' };
@@ -48,7 +79,6 @@ export class Editor extends React.Component {
 
         return <div>
             {out}
-
             <p>
                 {htmlStr}
             </p>
